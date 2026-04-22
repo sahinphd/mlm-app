19f17c2a2aa70cae7bb40bdf04d03251fbb6f61c
Sahin Ahmed <sahinahmed798@gmail.com>
2026-04-22 12:55:30 +0530

    registration referal code witten below

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -86,19 +86,7 @@
                 @enderror
             </div>
 
-            <!-- Referral -->
-            <div>
-              <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
-                Referral Code (Optional)
-              </label>
-              <input
-                type="text"
-                name="referral"
-                value="{{ request()->query('ref', old('referral', 'BJDWLPYM')) }}"
-                placeholder="Enter referral code"
-                class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
-              />
-            </div>
+           
 
             <!-- Password -->
             <div>
@@ -131,6 +119,20 @@
               />
             </div>
 
+             <!-- Referral -->
+            <div>
+              <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                Referral Code (Optional)
+              </label>
+              <input
+                type="text"
+                name="referral"
+                value="{{ request()->query('ref', old('referral', 'BJDWLPYM')) }}"
+                placeholder="Enter referral code"
+                class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+              />
+            </div>
+
             <!-- Checkbox -->
             <div class="flex items-center">
               <div x-data="{ checkboxToggle: false }">
@@ -145,7 +147,7 @@
                       </span>
                     </div>
                   </div>
-                  I accept the <a href="#" class="text-brand-500 ml-1">Terms & Conditions</a>
+                  I accept the <a href="#terms" class="text-brand-500 ml-1">Terms & Conditions</a>
                 </label>
               </div>
             </div>
