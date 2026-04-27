2ceaab3f4d3b5c57caec5627e44a511755718dc8
Sahin Ahmed <sahinahmed798@gmail.com>
2026-04-27 13:48:35 +0530

    80 20 register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -79,13 +79,13 @@
 
 
                     </select>
-                    <input
+                    <input                    
                       type="tel"
                       name="phone"
                       value="{{ old('phone') }}"
                       required
                       placeholder="8123456789"
-                      class="dark:bg-dark-900 h-11 W-inline flex-1 rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                      class="dark:bg-dark-900 h-11  w-80 W-inline flex-1 rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
                     />
                 </div>
                 @error('phone')
